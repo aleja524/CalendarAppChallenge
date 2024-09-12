793cb169173b4184faa179e86393559e4a25ae9a
Alejandra Escobar <aleja.escobar2501@gmail.com>
2024-09-12 11:35:32 -0500

    class event

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -17,12 +17,39 @@ class Reminder:
     type: str = EMAIL
 
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Reminder on {self.date_time} of type {self.type}"
 
 
 # TODO: Implement Event class here
+@dataclass
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
 
+    reminders: list[Reminder] = field(default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
+
+    def add_reminder(self, date_time: datetime, reminder_type: str):
+        reminder = Reminder(date_time=date_time, type=reminder_type)
+        self.reminders.append(reminder)
+
+    def delete_reminder(self, reminder_index: int):
+        if 0 <= reminder_index < len(self.reminders):
+            del self.reminders[reminder_index]
+        else:
+            reminder_not_found_error()
+
+    def __str__(self):
+        return(
+            f"ID: {self.id}\n"
+            f"Event title: {self.title}\n"
+            f"Description: {self.description}\n"
+            f"Time: {self.start_at} - {self.end_at}"
+        )
 
 # TODO: Implement Day class here
 
